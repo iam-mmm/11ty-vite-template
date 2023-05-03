9fd96e74bc2a7e94de85adca7e7aa4646a3780a6
matsumoto mika <mk.mtmt48@gmail.com>
2023-05-03 16:27:13 +0900

    日英対応 jsonでテスト中

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+const yaml = require("js-yaml");
+
 module.exports =  function (eleventyConfig) {
+    eleventyConfig.addDataExtension(
+        "yaml", contents => yaml.load(
+            fs.readFileSync(path + 'src/_data/messages_en.yml', 'utf-8')
+        )
+    );
+
     return {
         templateFormats: ['ejs', 'html'],
         htmlTemplateEngine: 'ejs',
